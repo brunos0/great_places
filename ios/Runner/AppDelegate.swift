a01ae53afab0a3b1257a562d7cbca3165eef96f0
Bruno Horvat <brunohorvat@Mac-MCC-C02G.local>
2023-11-17 10:57:40 -0300

    implementando api_key via env no AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,32 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let envChannel = FlutterMethodChannel(name: "com.example.great_places/iosbridge",
+                                          binaryMessenger: controller.binaryMessenger)
+    envChannel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+      guard call.method == "getApiKey" else {
+        result(FlutterMethodNotImplemented)
+        return
+      }
+      self.receiveApiKey(call: call, result: result)
+    })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func receiveApiKey(call: FlutterMethodCall, result: FlutterResult) {
+    guard let args = call.arguments else {
+      result("iOS could not recognize flutter arguments in method: (getApiKey)")
+      return
+    }
+    if let apiKey = args as? String {
+      GMSServices.provideAPIKey(apiKey)
+      result("API Key received")
+    } else {
+      result(FlutterError(code: "-1", message: "ERROR", details: nil))
+    }
   }
 }
