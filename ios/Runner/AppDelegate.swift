e296f10e2718484d85f59eb7977978978eec8be5
Bruno Horvat <brunohorvat@Mac-MCC-C02G.local>
2023-11-28 18:25:41 -0300

    Limpeza do código e remoção de dotenv

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,40 +4,26 @@ import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-
-      let envChannel = FlutterMethodChannel(name: "com.example.great_places/iosbridge",
-                                          binaryMessenger: controller.binaryMessenger)
-      
-    envChannel.setMethodCallHandler({
-      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-      guard call.method == "getApiKey" else {
-        result(FlutterMethodNotImplemented)
-        return
-      }
-      self.receiveApiKey(call: call, result: result)
-
-    })
-
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  private func receiveApiKey(call: FlutterMethodCall, result: FlutterResult) {
-    guard let args = call.arguments else {
-      result("iOS could not recognize flutter arguments in method: (getApiKey)")
-      return
+    
+    struct Environment {
+        static let infoDictionary: [String: Any] = {
+            guard let dict = Bundle.main.infoDictionary else {
+                fatalError("Arquivo Info.plist não encontrado")
+            }
+            return dict
+        }()
     }
-    if let apiKey = args as? String {
-      GMSServices.provideAPIKey(apiKey)
-      result("API Key received")
-    } else {
-      result(FlutterError(code: "-1", message: "ERROR", details: nil))
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        guard let apiKey = Environment.infoDictionary["API_KEY_MAPS_IOS"] as? String else {
+            fatalError("API_KEY_MAPS_IOS não definida no plist")
+        }
+        GMSServices.provideAPIKey(apiKey)
+        
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-  }
 }
